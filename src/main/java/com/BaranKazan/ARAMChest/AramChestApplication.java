31ac03f5c9c4dac7c94ea497594f962a46a90f49
Baran Kazan <Baran.kazan@hotmail.com>
2021-08-17 21:43:25 +0200

    Project init

---
+++ src/main/java/com/BaranKazan/ARAMChest/AramChestApplication.java
@@ -0,0 +1,13 @@
+package com.BaranKazan.ARAMChest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AramChestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AramChestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BaranKazan/ARAMChest/AramChestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BaranKazan.ARAMChest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AramChestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
