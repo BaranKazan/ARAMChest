62be8ce2a46197280edea5c730aaacaf0e3a76b6
Baran Kazan <baran.kazan@hotmail.com>
2021-11-16 15:31:30 +0100

    Fixed the issue with pom.xml

---
+++ src/main/java/com/BaranKazan/ARAMChest/Helper/AzureSecrets.java
@@ -0,0 +1,31 @@
+package com.BaranKazan.ARAMChest.Helper;
+
+import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.security.keyvault.secrets.SecretClient;
+import com.azure.security.keyvault.secrets.SecretClientBuilder;
+import com.azure.security.keyvault.secrets.models.KeyVaultSecret;
+
+import java.io.Console;
+import java.util.Map;
+
+public class AzureSecrets {
+
+    public static String getRiotApiKey() {
+        String keyVaultName = System.getenv("KEY_VAULT_NAME");
+        String keyVaultUri = "https://" + keyVaultName + ".vault.azure.net";
+
+        SecretClient secretClient = new SecretClientBuilder()
+                .vaultUrl(keyVaultUri)
+                .credential(new DefaultAzureCredentialBuilder().build())
+                .buildClient();
+
+        String secretName = "RIOT-API-KEY";
+        String secretValue = "";
+
+        KeyVaultSecret retrievedSecret = secretClient.getSecret(secretName);
+        secretValue = retrievedSecret.getValue();
+
+        return secretValue;
+    }
+
+}
