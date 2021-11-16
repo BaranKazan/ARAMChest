167bc125004fa1b568913a9c7baab533568d70da
Baran Kazan <baran.kazan@hotmail.com>
2021-11-16 15:19:09 +0100

    Removed name from Champion model

--- src/main/java/com/BaranKazan/ARAMChest/Champion/Champion.java
+++ src/main/java/com/BaranKazan/ARAMChest/Champion/Champion.java
@@ -3,22 +3,16 @@ package com.BaranKazan.ARAMChest.Champion;
 public class Champion {
 
     private final int id;
-    private final String name;
     private final boolean chestUnlocked;
 
-    public Champion(int id, String name, boolean chestUnlocked) {
+    public Champion(int id, boolean chestUnlocked) {
         this.id = id;
-        this.name = name;
         this.chestUnlocked = chestUnlocked;
     }
 
     public int getId() {
 
         return id;
-    }
-
-    public String getName(){
-        return name;
     }
 
     public boolean isChestUnlocked() {
