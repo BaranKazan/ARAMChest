309b5ceb5ace613fbdd4eac3496985c1e77cf262
Baran Kazan <Baran.kazan@hotmail.com>
2022-05-11 13:27:23 +0200

    Added riot.txt

--- src/main/java/com/BaranKazan/ARAMChest/exception/MyFileNotFoundException.java
+++ src/main/java/com/BaranKazan/ARAMChest/exception/MyFileNotFoundException.java
@@ -5,4 +5,8 @@ public class MyFileNotFoundException extends RuntimeException {
     public MyFileNotFoundException(String message) {
         super(message);
     }
+
+    public MyFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
--- src/main/java/com/BaranKazan/ARAMChest/summoner/SummonerController.java
+++ src/main/java/com/BaranKazan/ARAMChest/summoner/SummonerController.java
@@ -1,13 +1,21 @@
 package com.BaranKazan.ARAMChest.summoner;
 
+import com.BaranKazan.ARAMChest.exception.MyFileNotFoundException;
 import com.merakianalytics.orianna.types.common.Region;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 @RestController
 public class SummonerController {
-
     private final SummonerService summonerService;
 
     @Autowired
@@ -17,9 +25,30 @@ public class SummonerController {
 
     @RequestMapping(path = "/")
     public ModelAndView welcome() {
+        String pathName = "./../resources/static/index.html";
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("index.html");
+        modelAndView.setViewName(pathName);
         return modelAndView;
+    }
+
+    @RequestMapping(path="/riot.txt")
+    public ResponseEntity<InputStreamResource> riotTxt() {
+        String fileName = "./../resources/static/riot.txt";
+        File file;
+        InputStreamResource inputStreamResource;
+
+        try {
+            file = new File(fileName);
+            FileInputStream fileInputStream = new FileInputStream(file);
+            inputStreamResource = new InputStreamResource(fileInputStream);
+        } catch (FileNotFoundException e) {
+            throw new MyFileNotFoundException("The riot.txt file could not be found", e);
+        }
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + fileName)
+                .contentType(MediaType.TEXT_PLAIN)
+                .contentLength(file.length())
+                .body(inputStreamResource);
     }
 
     @GetMapping(path = "/summonerName/{summonerName}/region/{region}")
